f042900902991f10d60baa0f741099a464f6ad04
nomid0612 <nomid0612@gmail.com>
2022-12-26 12:57:19 +0300

    added index.d.ts

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 const httpRequest = require('./lib/httpRequest.js');
+const webSocketSendData = require('./lib/websocket.js');
 
 module.exports = { httpRequest };
+module.exports = { webSocketSendData };
