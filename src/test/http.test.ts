7b1b724fe370871f5237dce3538e8758e106dc72
nomid0612 <nomid0612@gmail.com>
2023-01-19 13:29:08 +0300

    added test for sendHttpRequest

---
+++ src/test/http.test.ts
@@ -0,0 +1,23 @@
+import {sendHttpRequest} from '../httpRequest';
+
+describe('sendHttpRequest tests', () => {
+    test('Check if GET Method works and return status, headers and response body', async () => {
+        const response = await sendHttpRequest('https://jsonplaceholder.typicode.com/todos/1', {method: 'GET'});
+        expect(response.status).toBe(200);
+        expect(response.statusText).toBe('OK');
+        expect(typeof response.headers).toBe('object');
+        expect(typeof response.payload).toBe('object');
+    });
+
+    test('Check if POST Method with request body works and return status, headers and response body', async () => {
+        const response = await sendHttpRequest('https://jsonplaceholder.typicode.com/posts', {method: 'POST', body:JSON.stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+            })});
+        expect(typeof response.status).toBe('number');
+        expect(typeof response.statusText).toBe('string');
+        expect(typeof response.headers).toBe('object');
+        expect(typeof response.payload).toBe('object');
+    });
+});
