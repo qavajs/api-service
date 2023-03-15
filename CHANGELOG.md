cc94503023210fcc664f3cfb60422f52af8824a0
Kiryl Bahdanets <Kiryl_Bahdanets@epam.com>
2023-03-15 10:23:40 +0300

    Added ws module as dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 0.0.7
+- fix issue with ws dependency
+
 ## 0.0.6
 - removed hardcoded terminal function from sendHttpRequest function
--- src/test/http.test.ts
+++ src/test/http.test.ts
@@ -1,16 +1,17 @@
+import { Response } from 'node-fetch';
 import { sendHttpRequest } from '../httpRequest';
 
 describe('sendHttpRequest tests', () => {
   test('Check if GET Method works and return status, headers and response body', async () => {
-    const response: any = await sendHttpRequest('https://jsonplaceholder.typicode.com/todos/1', { method: 'GET' });
+    const response: Response = await sendHttpRequest('https://jsonplaceholder.typicode.com/todos/1', { method: 'GET' });
     expect(response.status).toBe(200);
     expect(response.statusText).toBe('OK');
     expect(typeof response.headers).toBe('object');
-    expect(typeof response.payload).toBe('object');
+    expect(typeof response.body).toBe('object');
   });
 
   test('Check if POST Method with request body works and return status, headers and response body', async () => {
-    const response: any = await sendHttpRequest('https://jsonplaceholder.typicode.com/posts', {
+    const response: Response = await sendHttpRequest('https://jsonplaceholder.typicode.com/posts', {
       method: 'POST',
       body: JSON.stringify({
         title: 'foo',
@@ -21,6 +22,6 @@ describe('sendHttpRequest tests', () => {
     expect(typeof response.status).toBe('number');
     expect(typeof response.statusText).toBe('string');
     expect(typeof response.headers).toBe('object');
-    expect(typeof response.payload).toBe('object');
+    expect(typeof response.body).toBe('object');
   });
 });
